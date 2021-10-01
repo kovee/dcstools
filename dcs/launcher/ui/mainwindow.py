31d2f0d0a0e5bf46432c3dd43e5cad7d924e1cea
Attila Kovacs <kovee@suiseientertainment.com>
2021-10-01 19:09:53 +0200

    Starting to settle on main window layout.

--- dcs/launcher/ui/mainwindow.py
+++ dcs/launcher/ui/mainwindow.py
@@ -44,19 +44,102 @@ class DCSLauncherMainWindow(Window):
                                       height=self.Height)
 
         # Load Images
-        self._stripe = render = ImageTk.PhotoImage(
+        self._stripe = stripe = ImageTk.PhotoImage(
             file=os.path.abspath(os.path.expanduser(
                 './data/images/hazard_stripe.png')))
 
-        self._logo = render2 = ImageTk.PhotoImage(
+        self._logo = logo = ImageTk.PhotoImage(
             file=os.path.abspath(os.path.expanduser(
                 './data/images/logo.png')))
+
+        # Profile List
+        self._profile_list = ttk.Combobox(
+            self,
+            values=self.read_profile_list(),
+            width=70,
+            state='readonly')
+        self._profile_list.place(x=50, y=170)
+
+        # Profile Editor Button
+        self._profile_editor_button = ttk.Button(
+            self,
+            text='Edit Profile',
+            style='primary.TButton',
+            width=70,
+            command=self.on_edit_profile)
+        self._profile_editor_button.place(x=50, y=210)
+
+        # Profile Manager Button
+        self._profile_manager_button = ttk.Button(
+            self,
+            text='Profile Manager',
+            style='primary.TButton',
+            width=70,
+            command=self.on_profile_manager)
+        self._profile_manager_button.place(x=50, y=250)
+
+        # Launch Button
+        self._launch_button = ttk.Button(
+            self,
+            text='Launch',
+            style='danger.TButton',
+            width=70,
+            command=self.on_launch)
+        self._launch_button.place(x=50, y=290)
+
+        # Donate Button
+        self._donate_button = ttk.Button(
+            self,
+            text='Donate',
+            style='warning.TButton',
+            width=70,
+            command=self.on_donate)
+        self._donate_button.place(x=50, y=330)
+
+        # Settings Button
+        self._settings_button = ttk.Button(
+            self,
+            text='Settings',
+            style='secondary.TButton',
+            width=70,
+            command=self.on_settings)
+        self._settings_button.place(x=50,y=370)
+
+        # Exit Button
+        self._exit_button = ttk.Button(
+            self,
+            text='Exit',
+            style='secondary.TButton',
+            width=70,
+            command=self.on_exit)
+        self._exit_button.place(x=50, y=410)
 
         self._canvas.pack()
         self.pack()
 
-        self._canvas.create_image((0,-30), image=render, anchor='nw')
-        self._canvas.create_image((0,20), image=render2, anchor='nw')
-        self._canvas.create_image((0,975), image=render, anchor='nw')
+        self._canvas.create_image((0,-30), image=stripe, anchor='nw')
+        self._canvas.create_image((0,37), image=logo, anchor='nw')
+        self._canvas.create_image((0,465), image=stripe, anchor='nw')
 
         self.master.resizable(width=False, height=False)
+
+    def on_edit_profile(self) -> None:
+        pass
+
+    def on_profile_manager(self) -> None:
+        pass
+
+    def on_launch(self) -> None:
+        pass
+
+    def on_settings(self) -> None:
+        pass
+
+    def read_profile_list(self) -> list:
+        return  []
+
+    def on_exit(self) -> None:
+        self.master.destroy()
+
+    def on_donate(self) -> None:
+        pass
